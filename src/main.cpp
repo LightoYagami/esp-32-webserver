74d2921cbd29e52657f88faa1d1921b348024719
unknown <mjangid2349@gmail.com>
2023-02-24 17:57:35 +0530

    esp-32-webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #ifdef ESP8266
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 #elif defined(ESP32)
 #include <WiFi.h>
 #include <ESPmDNS.h>
 #else
+#error "Board not found"
 #endif
 
 #include <WebSocketsServer.h>
 
-#define LED1 13
+#include <ArduinoJson.h>
+
+
+
+#define LED1 15
 #define LED2 12
 
 char webpage[] PROGMEM = R"=====(
@@ -15,15 +22,36 @@ char webpage[] PROGMEM = R"=====(
 <html>
 <script>
 var connection = new WebSocket('ws://'+location.hostname+':81/');
+var button_1_status = 0;
+var button_2_status = 0;
 function button_1_on()
 {
+   button_1_status = 1; 
   console.log("LED 1 is ON");
-  connection.send("LED 1 is ON");
+  send_data();
 }
 function button_1_off()
 {
+  button_1_status = 0;
 console.log("LED 1 is OFF");
-connection.send("LED 1 is OFF");
+send_data();
+}
+function button_2_on()
+{
+   button_2_status = 1; 
+  console.log("LED 2 is ON");
+  send_data();
+}
+function button_2_off()
+{
+  button_2_status = 0;
+console.log("LED 2 is OFF");
+send_data();
+}
+function send_data()
+{
+  var full_data = '{"LED1" :'+button_1_status+',"LED2":'+button_2_status+'}';
+  connection.send(full_data);
 }
 </script>
 <body>
@@ -32,15 +60,16 @@ connection.send("LED 1 is OFF");
 <h3> LED 1 </h3>
 <button onclick= "button_1_on()" >On</button><button onclick="button_1_off()" >Off</button>
 <h3> LED 2 </h3>
-<button onclick="window.location = 'http://'+location.hostname+'/led2/on'">On</button><button onclick="window.location = 'http://'+location.hostname+'/led2/off'">Off</button>
+<button onclick="button_2_on()">On</button><button onclick="button_2_off()">Off</button>
 </center>
 </body>
 </html>
 )=====";
 
+
 #include <ESPAsyncWebServer.h>
 
-AsyncWebServer server(80); 
+AsyncWebServer server(80); // server port 80
 WebSocketsServer websockets(81);
 
 void notFound(AsyncWebServerRequest *request)
@@ -69,14 +98,21 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
       String message = String((char*)( payload));
       Serial.println(message);
 
-      if(message == "LED 1 is OFF"){
-        digitalWrite(LED1,LOW);
-      }
-
-      if(message == "LED 1 is ON"){
-        digitalWrite(LED1,HIGH);
-      }
       
+     DynamicJsonDocument doc(200);
+    DeserializationError error = deserializeJson(doc, message);
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+
+  int LED1_status = doc["LED1"];
+  int LED2_status = doc["LED2"];
+  digitalWrite(LED1,LED1_status);
+  digitalWrite(LED2,LED2_status);
+
+
 
 
   }
@@ -115,7 +151,7 @@ void setup(void)
 
   server.onNotFound(notFound);
 
-  server.begin();  // it will start webserver
+  server.begin();  
   websockets.begin();
   websockets.onEvent(webSocketEvent);
 
